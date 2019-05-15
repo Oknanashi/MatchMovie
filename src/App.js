cc221d21b6cd11d55363b1fd62cc8d0aca0d8a9c
Kirill Andreev <oknanashi@gmail.com>
2019-05-15 16:42:42 -0400

    majorFix

--- src/App.js
+++ src/App.js
@@ -117,8 +117,7 @@ class App extends React.Component {
         }
     }
   render(){
-        console.log(this.state.firstActorMovies)
-        console.log(this.state.sameMovies)
+
       return (
           <div className="App">
 
--- src/components/CommonMovie.js
+++ src/components/CommonMovie.js
@@ -1,15 +1,79 @@
 import React from 'react'
 import Loader from 'react-loader-spinner'
+import {Card,Box,Avatar,Text,Link,Button} from 'gestalt'
+import axios from 'axios'
+
+class CardExample extends React.Component {
+    state={
+        movie:{}
+    }
+    constructor(props) {
+        super(props);
+        this.state = { hovered: false };
+        this.handleMouseEnter = this._handleMouseEnter.bind(this);
+        this.handleMouseLeave = this._handleMouseLeave.bind(this);
+    }
+
+    async componentDidMount(){
+        await axios.get(`https://api.themoviedb.org/3/movie/${this.props.movie}?api_key=d4c86d3d23078bb5e3ea14ae379a2726&language=en-US`)
+            .then(response=>{
+                console.log(response)
+                this.setState({
+                    movie:response.data
+                })
+            })
+    }
+    _handleMouseEnter() {
+        this.setState(() => ({ hovered: true }));
+    }
+    _handleMouseLeave() {
+        this.setState(() => ({ hovered: false }));
+    }
+    render() {
+        console.log(this.state.movie)
+
+        if(this.state.movie){
+            return (
+                <Box maxWidth={236} padding={2} column={12}>
+                    <Card
+                        image={
+                            <Avatar
+                                name="James Jones"
+                                src={`https://image.tmdb.org/t/p/w500${this.state.movie.poster_path}`}
+                            />
+                        }
+                        onMouseEnter={this.handleMouseEnter}
+                        onMouseLeave={this.handleMouseLeave}>
+                        <Text align="center" bold size="xl">
+                            <Link href="https://pinterest.com">
+                                <Box paddingX={3} paddingY={2}>
+                                    {this.state.movie.original_title}
+                                </Box>
+                            </Link>
+                        </Text>
+                        <Button
+                            accessibilityLabel="Follow James Jones"
+                            color="red"
+                            text="Look on tmdb"
+                        />
+                    </Card>
+                </Box>
+            );
+        }
+        else{
+            return(
+                <div>None</div>
+            )
+        }
+    }
+}
 
 export default class CommonMovies extends React.Component{
 
 
-    async componentDidMount(){
-        if(this.props.sameMovies.length>1){
 
-        }
-    }
     render(){
+        console.log(this.props.sameMovies)
         return(
             <div>
                 <h1>No common Movie</h1>
@@ -22,6 +86,9 @@ export default class CommonMovies extends React.Component{
                     />
                 )}
                 <button onClick={this.props.startMatching}>Start Matching</button>
+                {this.props.sameMovies.map(movie=>(
+                    <CardExample movie={movie}> </CardExample>
+                ))}
             </div>
 
         )
