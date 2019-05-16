e39e400f0932b4a1c515e07e2962df4d7f7cc33f
Kirill Andreev <oknanashi@gmail.com>
2019-05-16 11:13:05 -0400

    Updates

---
+++ precache-manifest.d0f0f7ee309e173a5d6413d2cb33101a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c63aaf4ae2043964cad54afda0dcb5f",
+    "url": "/Movie-match/index.html"
+  },
+  {
+    "revision": "fcb19eb168d8e353db25",
+    "url": "/Movie-match/static/css/2.c8f966f6.chunk.css"
+  },
+  {
+    "revision": "2c7c8d20077367b802c1",
+    "url": "/Movie-match/static/css/main.38a330e7.chunk.css"
+  },
+  {
+    "revision": "fcb19eb168d8e353db25",
+    "url": "/Movie-match/static/js/2.14afbee6.chunk.js"
+  },
+  {
+    "revision": "2c7c8d20077367b802c1",
+    "url": "/Movie-match/static/js/main.36a0b797.chunk.js"
+  },
+  {
+    "revision": "743ecfc02585d1e3b95d",
+    "url": "/Movie-match/static/js/runtime~main.774bcce8.js"
+  }
+]);
